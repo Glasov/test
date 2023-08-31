024739a024ecdb07bd0a39467425217bb28100bf
Admin user <adminuser@admin.local>
2023-08-31 16:19:06 +0300

    test

---
+++ Entropy.py
@@ -0,0 +1,22 @@
+import math 
+class Entropy: 
+    def __init__(self, number_all, number_white, number_black, number_red):
+        self.number_all = number_all
+        self.number_white = number_white
+        self.number_black = number_black
+        self.number_red = number_red
+
+    def probability(self, value):
+        return value/self.number_all
+
+    def log2(self, value):
+        return math.log10(value)
+
+    def entropy(self, array_values):
+        res = 0
+        for i in range(3):
+            res -= (
+                self.probability(array_values[i]) *
+                self.log2(self.probability(array_values[i]))
+            )
+            return res
---
+++ Main.py
@@ -0,0 +1,23 @@
+from Entropy import *
+
+number_all = 0
+experiment1 = []
+experiment2 = []
+number_all = int(input())
+experiment1 = [int(i) for i in input().split()]
+experiment2 = [int(i) for i in input().split()]
+
+entr_arr1 = Entropy(number_all, *experiment1)
+entr_arr2 = Entropy(number_all, *experiment2)
+
+experiment1_for_entropy1 = [entr_arr1.number_white, entr_arr1.number_black, entr_arr1.number_red]
+experiment2_for_entropy2 = [entr_arr2.number_white, entr_arr2.number_black, entr_arr2.number_red]
+entr1 = entr_arr1.entropy(experiment1_for_entropy1)
+entr2 = entr_arr2.entropy(experiment2_for_entropy2)
+
+if entr2 > entr1:
+    print("2-й опыт имеет большую неопределенность, чем первый")
+elif entr1 > entr2:
+    print("1-й опыт имеет большую неопределенность, чем второй")
+else:
+    print("1-й и 2-й опыты имеют одинаковую неопределенность")
